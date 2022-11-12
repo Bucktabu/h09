4b59098dc35820d519a3701091ebc0e5599be685
Bucktabu <stanislaworonin@yandex.ru>
2022-11-12 11:45:32 +0300

    add ipAddress limiter v6

--- src/middlewares/validation-middleware/ipAddressLimiter.ts
+++ src/middlewares/validation-middleware/ipAddressLimiter.ts
@@ -3,9 +3,9 @@ import {ipAddressCollection} from "../../repositories/db";
 
 export const ipAddressLimiter = async (req: Request, res: Response, next: NextFunction) => {
     const ip = req.ip
-    const connectionsCount = await ipAddressCollection.countDocuments({ipAddress: ip, connectionAt: {$gte: (Date.now() - 10000)}})
-    if (connectionsCount > 4) return res.sendStatus(429)
     await ipAddressCollection.insertOne({ipAddress: ip, connectionAt: Date.now()})
+    const connectionsCount = await ipAddressCollection.countDocuments({ipAddress: ip, connectionAt: {$gte: (Date.now() - 10000)}})
+    if (connectionsCount > 5) return res.sendStatus(429)
     return next()
     // const ipAddress = await ipAddressCollection.findOne({ipAddress: req.ip})
     //
